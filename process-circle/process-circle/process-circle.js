1a7ab164cebd96d18c21bbde7a69e73fdde620d8
Jehorn <gerardgu@outlook.com>
2018-01-14 18:14:06 +0800

    Circle Process @v 1.4.0 Add 'set' method and transition animation.

--- process-circle/process-circle/process-circle.js
+++ process-circle/process-circle/process-circle.js
@@ -1,17 +1,71 @@
 /*!
  * Circle Process
- * @version 1.3.5
+ * @version 1.4.0
  * @author Jehorn(gerardgu@outlook.com)
  * IE9/IE9+
+ * ======================================
+ * 由于IE的SetTimeOut最小时间限制，会出现环形进度条与数字变化效果不一致的情况
+ * speed设置小于等于100时没有过渡效果
+ * Chrome浏览器下size设置为奇数时，底部会出现漏色。建议使用偶数
  */
 
 ;
 (function (window, document) {
+    'use strict';
+
+    // 内部计数器
+    var _counts = 0;
+
+    // 常量
+    var constants = {
+        CLASS_NAME: {
+            WRAPPER: 'wrapper',
+            RIGHT: 'circle-right',
+            LEFT: 'circle-left',
+            CONTENT: 'content',
+            CIRCLE: 'circle',
+            PREPEND: 'prepend',
+            APPEND: 'append',
+            PERCENT_SYMBOL: 'symbol-percent',
+            PERCENT_BOX: 'percent-box'
+        },
+        ID_NAME: {
+            RIGHT: 'circleRight',
+            LEFT: 'circleLeft',
+            PERCENT: 'percentProcess'
+        },
+        PERCENT_INIT: '0%',
+        CIRCLE_STYLES: {
+            borderRadius: '50%',
+            position: 'absolute',
+            top: '0',
+            transform: 'rotate(-135deg)',
+            msTransform: 'rotate(-135deg)',
+            mozTransform: 'rotate(-135deg)',
+            webkitTransform: 'rotate(-135deg)',
+            oTransform: 'rotate(-135deg)'
+        },
+        CONTENT_STYLES: {
+            textAlign: 'center',
+            position: 'absolute',
+            width: '100%',
+            top: '50%',
+            transform: 'translateY(-50%)',
+            msTransform: 'translateY(-50%)',
+            mozTransform: 'translateY(-50%)',
+            webkitTransform: 'translateY(-50%)',
+            oTransform: 'translateY(-50%)'
+        },
+        NO_ANIMATE_TIME: 100,
+        IS_INTERVAL_DEFAULT: false
+
+    }
+
     var ProcessCircle = function (json) {
         if (this instanceof ProcessCircle) {
             this.author = 'Jehorn';
-            this.version = '1.3.5';
-			
+            this.version = '1.4.0';
+
             // 进度条的宽/高
             this.size = json.size || 100;
             // 边框的宽度
@@ -23,7 +77,7 @@
             // 显示的百分比值
             this.num = json.num || 0;
             // 动画速度 毫秒
-            this.speed = json.speed || 10;
+            this.speed = json.speed || 600;
 
             // 容器的 选择器
             this.selector = json.selector || '.process-circle';
@@ -42,7 +96,9 @@
             // 是否需要实例化完毕自动渲染
             // 如果为 false, 需要在实例化完毕后调用 init 方法
             this.isInit = json.isInit === false ? false : true;
-			
+            // 是否直接使用setInterval
+            this.isInterval = json.isInterval === !constants.IS_INTERVAL_DEFAULT ? !constants.IS_INTERVAL_DEFAULT : constants.IS_INTERVAL_DEFAULT;
+
 			// 追加内容
 			this.prepend = json.prepend || '';
 			this.append = json.append || '';
@@ -56,52 +112,6 @@
         }
     }
 
-	// 内部计数器
-	var _counts = 0;
-	
-	// 常量
-	var constants = {
-		CLASS_NAME: {
-			WRAPPER: 'wrapper',
-			RIGHT: 'circle-right',
-			LEFT: 'circle-left',
-			CONTENT: 'content',
-			CIRCLE: 'circle',
-			PREPEND: 'prepend',
-            APPEND: 'append',
-            PERCENT_SYMBOL: 'symbol-percent',
-            PERCENT_BOX: 'percent-box'
-		},
-		ID_NAME: {
-			RIGHT: 'circleRight',
-			LEFT: 'circleLeft',
-			PERCENT: 'percentProcess'
-		},
-		PERCENT_INIT: '0%',
-		CIRCLE_STYLES: {
-			borderRadius: '50%',
-			position: 'absolute',
-			top: '0',
-			transform: 'rotate(-135deg)',
-			msTransform: 'rotate(-135deg)',
-			mozTransform: 'rotate(-135deg)',
-			webkitTransform: 'rotate(-135deg)',
-			oTransform: 'rotate(-135deg)'
-		},
-		CONTENT_STYLES: {
-			textAlign: 'center',
-			position: 'absolute',
-			width: '100%',
-			top: '50%',
-			transform: 'translateY(-50%)',
-			msTransform: 'translateY(-50%)',
-			mozTransform: 'translateY(-50%)',
-			webkitTransform: 'translateY(-50%)',
-			oTransform: 'translateY(-50%)'
-		}
-		
-	}
-	
 	// 控制台打印信息
     var debug = function (_this, json) {
         this.info = json.info || '';
@@ -162,8 +172,11 @@
         }
     }
 
-	
+	// 公共工具
     var utils = new (function () {
+        var self = this;
+        this.isIE9 = false;
+
         // 是否非空数组
         this.isArray = function (attr, callback) {
             var isArr = false;
@@ -233,6 +246,21 @@
 			ele.style.webkitTransform = value;
 			ele.style.oTransform = value;
 		}
+
+		// CSS3 transition 兼容性设置
+        this.transitionCompatibility = function (ele, value) {
+            ele.style.transition = value;
+            ele.style.mozTransition = value;
+            ele.style.webkitTransition = value;
+            ele.style.oTransition = value;
+        }
+
+        // 判断是否IE9
+        this._isIE9 = function () {
+            if (navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.match(/9./i)=="9.") {
+                self.isIE9 = true;
+            }
+        }();
 		
 		// 传入值验证合法性
 		// 自动转换
@@ -369,7 +397,7 @@
 		}
 		
 	})();
-	
+
     // 创建DOM
     var create = function (_this, index) {
 		var docFragment = document.createDocumentFragment();
@@ -453,7 +481,7 @@
         var bgBorder = border_size + _this.unit + ' ' + border_type + ' ' + border_bg_color;
 		
         for (var i = 0; i < wrappers.length; i++) {
-            wrappers[i].style.width = wrapper_size + _this.unit;
+            // wrappers[i].style.width = wrapper_size + _this.unit;
             wrappers[i].style.height = wrapper_size_full + _this.unit;
 			wrappers[i].style.position = 'absolute';
 		    wrappers[i].style.top = '0';
@@ -463,16 +491,20 @@
             circles[i].style.border = bgBorder;
 			utils.setStyles(circles[i], constants.CIRCLE_STYLES);
         }
-		
+
+        doms.right.style.width = Math.floor(wrapper_size) + _this.unit;
+        doms.left.style.width = Math.ceil(wrapper_size) + _this.unit;
 		doms.right.style.right = '0';
 		doms.left.style.left = '0';
 
         doms.right_in.style.borderTopColor = utils.getText(border_color, doms.index);
         doms.right_in.style.borderRightColor = utils.getText(border_color, doms.index);
 		doms.right_in.style.right = '0';
+        utils.transitionCompatibility(doms.right_in, 'transform ' + doms.speed / 2 + 'ms linear');
         doms.left_in.style.borderBottomColor = utils.getText(border_color, doms.index);
         doms.left_in.style.borderLeftColor = utils.getText(border_color, doms.index);
 		doms.left_in.style.left = '0';
+        utils.transitionCompatibility(doms.left_in, 'transform ' + doms.speed / 2 + 'ms linear');
 		
 		utils.setStyles(doms.content, constants.CONTENT_STYLES);
         doms.content.style.fontSize = text_size + _this.unit;
@@ -489,30 +521,89 @@
 		doms.container.appendChild(doms.doc_fragment);
     }
 
-    // 设置百分比
-    var setPercent = function (_this, doms) {
-        var num = parseFloat(utils.getText(_this.num, doms.index));
-        var speed = parseFloat(utils.getText(_this.speed, doms.index));
+    // percent animate
+    var setPercentNumber = function (_this, doms) {
         var i = 0;
 
-        var clock = window.setInterval(function () {
+        if (doms.speed <= constants.NO_ANIMATE_TIME) {
+            doms.percent.innerHTML = doms.num + '%';
+            return;
+        }
+
+        var timer = window.setInterval(function () {
             i++;
-            if (i > num) {
-                i = num;
-                window.clearInterval(clock);
+            if (i > doms.num) {
+                i = doms.num;
+                window.clearInterval(timer);
             }
             // _this.isText ? doms.percent.innerHTML = i + '<span class="' + constants.CLASS_NAME.PERCENT_SYMBOL + '">%</span>' : '';
             _this.isText ? doms.percent.innerHTML = i + '%' : '';
+        }, doms.speed / doms.num);
+    }
 
-            if (i <= 50) {
-				utils.transformCompatibility(doms.right_in, 'rotate(' + (-135 + 3.6 * i) + 'deg)');
-				utils.transformCompatibility(doms.left_in, 'rotate(-135deg)');
+    // async to animate
+    var setPercentCircle = function (_this, doms) {
+
+        if (doms.num <= 50) {
+            utils.transitionCompatibility(doms.right_in, 'transform ' + doms.speed + 'ms linear');
+        }
+
+        if (doms.speed <= constants.NO_ANIMATE_TIME) {
+            utils.transitionCompatibility(doms.right_in, 'transform 0ms linear');
+            utils.transitionCompatibility(doms.left_in, 'transform 0ms linear');
+        }
+
+        window.setTimeout(function() {
+            if (doms.num <= 50) {
+                utils.transformCompatibility(doms.right_in, 'rotate(' + (-135 + 3.6 * doms.num) + 'deg)');
+                utils.transformCompatibility(doms.left_in, 'rotate(-135deg)');
             } else {
-				utils.transformCompatibility(doms.right_in, 'rotate(45deg)');
-				utils.transformCompatibility(doms.left_in, 'rotate(' + (-135 + 3.6 * (i - 50)) + 'deg)');
-            }
+                utils.transformCompatibility(doms.right_in, 'rotate(45deg)');
 
-        }, speed);
+                if (doms.speed <= constants.NO_ANIMATE_TIME) {
+                    utils.transformCompatibility(doms.left_in, 'rotate(' + (-135 + 3.6 * (doms.num - 50)) + 'deg)');
+                    return;
+                }
+
+                setTimeout(function() {
+                    utils.transformCompatibility(doms.left_in, 'rotate(' + (-135 + 3.6 * (doms.num - 50)) + 'deg)');
+                }, doms.speed / 2);
+            }
+        }, 0);
+    }
+
+    // 设置百分比及效果
+    var setPercent = function (_this, doms) {
+        if (utils.isIE9 || _this.isInterval) {
+            var i = 0;
+
+            utils.transitionCompatibility(doms.right_in, 'transform ' + 0 + 'ms linear');
+            utils.transitionCompatibility(doms.left_in, 'transform ' + 0 + 'ms linear');
+
+            var clock = window.setInterval(function () {
+                i++;
+                if (i > doms.num) {
+                    i = doms.num;
+                    window.clearInterval(clock);
+                }
+                // _this.isText ? doms.percent.innerHTML = i + '<span class="' + constants.CLASS_NAME.PERCENT_SYMBOL + '">%</span>' : '';
+                _this.isText ? doms.percent.innerHTML = i + '%' : '';
+
+                if (i <= 50) {
+                    utils.transformCompatibility(doms.right_in, 'rotate(' + (-135 + 3.6 * i) + 'deg)');
+                    utils.transformCompatibility(doms.left_in, 'rotate(-135deg)');
+                } else {
+                    utils.transformCompatibility(doms.right_in, 'rotate(45deg)');
+                    utils.transformCompatibility(doms.left_in, 'rotate(' + (-135 + 3.6 * (i - 50)) + 'deg)');
+                }
+
+            }, doms.speed / doms.num);
+
+            return;
+        }
+
+        setPercentCircle(_this, doms);
+        setPercentNumber(_this, doms);
     }
     
     ProcessCircle.prototype = {
@@ -539,27 +630,56 @@
 
             this.containers = containers;
             this.counts = containers.length;
+            this.doms = [];
             for (var i = this.counts - 1; i >= 0; i--) {
                 var doms = create(this, i);
                 if (doms) {
                     doms.container = containers[i];
                     doms.index = i;
+                    doms.speed = parseFloat(utils.getText(this.speed, doms.index));
+                    doms.num = parseFloat(utils.getText(this.num, doms.index));
                     setStyle(this, doms);
                     setPercent(this, doms);
                 }
+
+                this.doms.push(doms);
             }
 
             return this;
         },
 
-        // TODO: 设置百分比
-        set: function (index) {
-            index = +index;
-            index = isNaN(index) ? 0 : index;
+        // 设置百分比
+        set: function (index, percent) {
+            var self = this;
+            var is_arr = utils.isArray(index);
+            var is_num = function (val) {
+                val = +val;
+                val = isNaN(val) ? 0 : val;
 
-            var percent = this.containers[index].querySelector('.' + constants.CLASS_NAME.PERCENT_BOX);
+                return val;
+            };
+            var set_num = function (index, val) {
+               if (utils.isArray(self.num)) {
+                   self.num[index] = val;
+                   return;
+               }
 
-            console.log(percent);
+                self.num = val;
+            }
+
+            if (is_arr) {
+                for (var i = 0; i < index.length; i++) {
+                    var _p = utils.getText(percent, i);
+                    var _i = is_num(index[i]);
+                    set_num(_i, _p);
+                    // this.num[_i] = _p;
+                    this.init();
+                }
+            } else {
+                index = is_num(index);
+                set_num(index, percent);
+                this.init();
+            }
         }
     }
 
