f13e28732fa6de25ac16eb8f4bdf3b565e21737d
Jehorn <gerardgu@outlook.com>
2018-01-15 09:28:13 +0800

    Circle Process @version 1.4.1

--- process-circle/process-circle/process-circle.js
+++ process-circle/process-circle/process-circle.js
@@ -1,6 +1,6 @@
 /*!
  * Circle Process
- * @version 1.4.0
+ * @version 1.4.1
  * @author Jehorn(gerardgu@outlook.com)
  * IE9/IE9+
  * ======================================
@@ -64,7 +64,7 @@
     var ProcessCircle = function (json) {
         if (this instanceof ProcessCircle) {
             this.author = 'Jehorn';
-            this.version = '1.4.0';
+            this.version = '1.4.1';
 
             // 进度条的宽/高
             this.size = json.size || 100;
@@ -99,9 +99,9 @@
             // 是否直接使用setInterval
             this.isInterval = json.isInterval === !constants.IS_INTERVAL_DEFAULT ? !constants.IS_INTERVAL_DEFAULT : constants.IS_INTERVAL_DEFAULT;
 
-			// 追加内容
-			this.prepend = json.prepend || '';
-			this.append = json.append || '';
+            // 追加内容
+            this.prepend = json.prepend || '';
+            this.append = json.append || '';
 
             if (this.isInit) {
                 this.init();
@@ -112,7 +112,7 @@
         }
     }
 
-	// 控制台打印信息
+    // 控制台打印信息
     var debug = function (_this, json) {
         this.info = json.info || '';
         this.type = json.type || 'log';
@@ -172,7 +172,7 @@
         }
     }
 
-	// 公共工具
+    // 公共工具
     var utils = new (function () {
         var self = this;
         this.isIE9 = false;
@@ -195,33 +195,32 @@
         // 删除容器下的所有元素
         this.delChildren = function (ele) {
             if (!ele) return;
-            while (ele.hasChildNodes())
-            {
+            while (ele.hasChildNodes()) {
                 ele.removeChild(ele.firstChild);
             }
         }
 
-		// 设置dom的className
-		this.setClasses = function (ele, classnames) {
-			var classes = '';
-			if (typeof classnames === 'string') {
-				classes = classnames;
-			} else if (typeof classnames === 'object' && classnames.length) {
-				for (var len = classnames.length, i = len - 1; i >= 0; i--) {
-					classes += classnames[i] + ' ';
-				}
-			}
-			
-			ele.className = classes;
-		}
+        // 设置dom的className
+        this.setClasses = function (ele, classnames) {
+            var classes = '';
+            if (typeof classnames === 'string') {
+                classes = classnames;
+            } else if (typeof classnames === 'object' && classnames.length) {
+                for (var len = classnames.length, i = len - 1; i >= 0; i--) {
+                    classes += classnames[i] + ' ';
+                }
+            }
+
+            ele.className = classes;
+        }
 
         // 设置dom的style
-		this.setStyles = function (ele, styles) {
-			if (typeof styles === 'object' && !styles.length) {
-				for (var style in styles) {
-					ele.style[style] = styles[style];
-				}
-			}
+        this.setStyles = function (ele, styles) {
+            if (typeof styles === 'object' && !styles.length) {
+                for (var style in styles) {
+                    ele.style[style] = styles[style];
+                }
+            }
         }
 
         // 获取数组/字符串属性值
@@ -237,17 +236,17 @@
 
             return text;
         }
-        
-		// CSS3 transform 兼容性设置
-		this.transformCompatibility = function (ele, value) {
-			ele.style.transform = value;
-			ele.style.msTransform = value;
-			ele.style.mozTransform = value;
-			ele.style.webkitTransform = value;
-			ele.style.oTransform = value;
-		}
 
-		// CSS3 transition 兼容性设置
+        // CSS3 transform 兼容性设置
+        this.transformCompatibility = function (ele, value) {
+            ele.style.transform = value;
+            ele.style.msTransform = value;
+            ele.style.mozTransform = value;
+            ele.style.webkitTransform = value;
+            ele.style.oTransform = value;
+        }
+
+        // CSS3 transition 兼容性设置
         this.transitionCompatibility = function (ele, value) {
             ele.style.transition = value;
             ele.style.mozTransition = value;
@@ -257,21 +256,21 @@
 
         // 判断是否IE9
         this._isIE9 = function () {
-            if (navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.match(/9./i)=="9.") {
+            if (navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.match(/9./i) == "9.") {
                 self.isIE9 = true;
             }
         }();
-		
-		// 传入值验证合法性
-		// 自动转换
-		this.checkParam = function (_this) {
-			var errors = [];
 
-			var float = /^(-?\d+)(\.\d+)?$/;
-			var floatPlus = /^\d+(\.\d+)?$/;
-			var color = /^#[0-9a-fA-F]{3,6}$/;
-			var units = ['px', 'em', 'rem'];
-			var borderStyle = ['none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset', 'inherit'];
+        // 传入值验证合法性
+        // 自动转换
+        this.checkParam = function (_this) {
+            var errors = [];
+
+            var float = /^(-?\d+)(\.\d+)?$/;
+            var floatPlus = /^\d+(\.\d+)?$/;
+            var color = /^#[0-9a-fA-F]{3,6}$/;
+            var units = ['px', 'em', 'rem'];
+            var borderStyle = ['none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset', 'inherit'];
 
             var checkIsArrAttr = function (attr, check) {
                 utils.isArray(attr, function (isArr) {
@@ -284,12 +283,12 @@
                     }
                 });
             }
-            
-			if (units.indexOf(_this.unit) < 0) {
-				var error = { param: 'unit', msg: '属性包括[\'px\', \'em\', \'rem\']' };
-				errors.push(error);
+
+            if (units.indexOf(_this.unit) < 0) {
+                var error = { param: 'unit', msg: '属性包括[\'px\', \'em\', \'rem\']' };
+                errors.push(error);
             }
-            
+
             // size
             var checkSize = function (size) {
                 if (!floatPlus.test(size)) {
@@ -309,7 +308,7 @@
             }
 
             checkIsArrAttr(_this.borderSize, checkBorderSize);
-            
+
             // textSize
             var checkTextSize = function (textSize) {
                 if (!floatPlus.test(textSize)) {
@@ -344,7 +343,7 @@
             };
 
             checkIsArrAttr(_this.speed, checkSpeed);
-			
+
             // textColor
             var checkTextColor = function (textColor) {
                 if (!color.test(textColor)) {
@@ -354,7 +353,7 @@
             };
 
             checkIsArrAttr(_this.textColor, checkTextColor);
-			
+
             // borderColor
             var checkBorderColor = function (borderColor) {
                 if (!color.test(borderColor)) {
@@ -384,33 +383,33 @@
             };
 
             checkIsArrAttr(_this.borderType, checkBorderType);
-			
-			if (errors.length > 0) {
-				debug(_this, {
-					info: errors,
-					type: 'error'
-				});
-				return false;
-			}
 
-			return true;
-		}
-		
-	})();
+            if (errors.length > 0) {
+                debug(_this, {
+                    info: errors,
+                    type: 'error'
+                });
+                return false;
+            }
+
+            return true;
+        }
+
+    })();
 
     // 创建DOM
     var create = function (_this, index) {
-		var docFragment = document.createDocumentFragment();
-		var class_names = constants.CLASS_NAME,
-			id_names = constants.ID_NAME;
-		
-		var right = document.createElement('div'),
-			left = document.createElement('div'),
-			content = document.createElement('div');
-		var right_in = document.createElement('div'),
-			left_in = document.createElement('div'),
-			prepend = document.createElement('span'),
-			percent = document.createElement('span'),
+        var docFragment = document.createDocumentFragment();
+        var class_names = constants.CLASS_NAME,
+            id_names = constants.ID_NAME;
+
+        var right = document.createElement('div'),
+            left = document.createElement('div'),
+            content = document.createElement('div');
+        var right_in = document.createElement('div'),
+            left_in = document.createElement('div'),
+            prepend = document.createElement('span'),
+            percent = document.createElement('span'),
             append = document.createElement('span');
 
         if (_this.counts > 1) {
@@ -421,47 +420,47 @@
                 append_t = document.createTextNode(_this.append);
         }
         var percent_t = document.createTextNode(constants.PERCENT_INIT);
-		
-		utils.setClasses(right, [class_names.WRAPPER, class_names.RIGHT]);
-		utils.setClasses(left, [class_names.WRAPPER, class_names.LEFT]);
-		utils.setClasses(content, class_names.CONTENT);
-		utils.setClasses(right_in, class_names.CIRCLE);
-		utils.setClasses(left_in, class_names.CIRCLE);
-		utils.setClasses(prepend, class_names.PREPEND);
-        utils.setClasses(append, class_names.APPEND); 
+
+        utils.setClasses(right, [class_names.WRAPPER, class_names.RIGHT]);
+        utils.setClasses(left, [class_names.WRAPPER, class_names.LEFT]);
+        utils.setClasses(content, class_names.CONTENT);
+        utils.setClasses(right_in, class_names.CIRCLE);
+        utils.setClasses(left_in, class_names.CIRCLE);
+        utils.setClasses(prepend, class_names.PREPEND);
+        utils.setClasses(append, class_names.APPEND);
         utils.setClasses(percent, class_names.PERCENT_BOX);
-		
-		right_in.id = id_names.RIGHT + '_' + _counts;
-		left_in.id = id_names.LEFT + '_' + _counts;
-		percent.id = id_names.PERCENT + '_' + _counts;
-		
+
+        right_in.id = id_names.RIGHT + '_' + _counts;
+        left_in.id = id_names.LEFT + '_' + _counts;
+        percent.id = id_names.PERCENT + '_' + _counts;
+
         prepend.appendChild(prepend_t);
-		append.appendChild(append_t);
+        append.appendChild(append_t);
         _this.isText ? percent.appendChild(percent_t) : '';
-		
-		right.appendChild(right_in);
-		left.appendChild(left_in);
-		content.appendChild(prepend);
-		content.appendChild(percent);
-		content.appendChild(append);
-		
-		docFragment.appendChild(right);
-		docFragment.appendChild(left);
-		docFragment.appendChild(content);
-		
-		_counts++;
-		
+
+        right.appendChild(right_in);
+        left.appendChild(left_in);
+        content.appendChild(prepend);
+        content.appendChild(percent);
+        content.appendChild(append);
+
+        docFragment.appendChild(right);
+        docFragment.appendChild(left);
+        docFragment.appendChild(content);
+
+        _counts++;
+
         return {
-			right: right,
-			left: left,
-			content: content,
-			right_in: right_in,
-			left_in: left_in,
-			prepend: prepend,
-			append: append,
-			percent: percent,
-			doc_fragment: docFragment
-		};
+            right: right,
+            left: left,
+            content: content,
+            right_in: right_in,
+            left_in: left_in,
+            prepend: prepend,
+            append: append,
+            percent: percent,
+            doc_fragment: docFragment
+        };
     }
 
     // 设置样式
@@ -479,46 +478,46 @@
         var wrappers = [doms.right, doms.left],
             circles = [doms.right_in, doms.left_in];
         var bgBorder = border_size + _this.unit + ' ' + border_type + ' ' + border_bg_color;
-		
+
         for (var i = 0; i < wrappers.length; i++) {
             // wrappers[i].style.width = wrapper_size + _this.unit;
             wrappers[i].style.height = wrapper_size_full + _this.unit;
-			wrappers[i].style.position = 'absolute';
-		    wrappers[i].style.top = '0';
-		    wrappers[i].style.overflow = 'hidden';
+            wrappers[i].style.position = 'absolute';
+            wrappers[i].style.top = '0';
+            wrappers[i].style.overflow = 'hidden';
             circles[i].style.width = circle_size + _this.unit;
             circles[i].style.height = circle_size + _this.unit;
             circles[i].style.border = bgBorder;
-			utils.setStyles(circles[i], constants.CIRCLE_STYLES);
+            utils.setStyles(circles[i], constants.CIRCLE_STYLES);
         }
 
         doms.right.style.width = Math.floor(wrapper_size) + _this.unit;
         doms.left.style.width = Math.ceil(wrapper_size) + _this.unit;
-		doms.right.style.right = '0';
-		doms.left.style.left = '0';
+        doms.right.style.right = '0';
+        doms.left.style.left = '0';
 
         doms.right_in.style.borderTopColor = utils.getText(border_color, doms.index);
         doms.right_in.style.borderRightColor = utils.getText(border_color, doms.index);
-		doms.right_in.style.right = '0';
+        doms.right_in.style.right = '0';
         utils.transitionCompatibility(doms.right_in, 'transform ' + doms.speed / 2 + 'ms linear');
         doms.left_in.style.borderBottomColor = utils.getText(border_color, doms.index);
         doms.left_in.style.borderLeftColor = utils.getText(border_color, doms.index);
-		doms.left_in.style.left = '0';
+        doms.left_in.style.left = '0';
         utils.transitionCompatibility(doms.left_in, 'transform ' + doms.speed / 2 + 'ms linear');
-		
-		utils.setStyles(doms.content, constants.CONTENT_STYLES);
+
+        utils.setStyles(doms.content, constants.CONTENT_STYLES);
         doms.content.style.fontSize = text_size + _this.unit;
         doms.content.style.color = text_color;
-		
-		doms.prepend.style.display = 'block';
+
+        doms.prepend.style.display = 'block';
         doms.append.style.display = 'block';
-        
+
         doms.container.style.width = wrapper_size_full + _this.unit;
         doms.container.style.height = wrapper_size_full + _this.unit;
         doms.container.style.position = 'relative';
 
         utils.delChildren(doms.container);
-		doms.container.appendChild(doms.doc_fragment);
+        doms.container.appendChild(doms.doc_fragment);
     }
 
     // percent animate
@@ -553,7 +552,7 @@
             utils.transitionCompatibility(doms.left_in, 'transform 0ms linear');
         }
 
-        window.setTimeout(function() {
+        window.setTimeout(function () {
             if (doms.num <= 50) {
                 utils.transformCompatibility(doms.right_in, 'rotate(' + (-135 + 3.6 * doms.num) + 'deg)');
                 utils.transformCompatibility(doms.left_in, 'rotate(-135deg)');
@@ -565,7 +564,7 @@
                     return;
                 }
 
-                setTimeout(function() {
+                setTimeout(function () {
                     utils.transformCompatibility(doms.left_in, 'rotate(' + (-135 + 3.6 * (doms.num - 50)) + 'deg)');
                 }, doms.speed / 2);
             }
@@ -605,7 +604,7 @@
         setPercentCircle(_this, doms);
         setPercentNumber(_this, doms);
     }
-    
+
     ProcessCircle.prototype = {
         // 初始化
         init: function () {
@@ -659,30 +658,45 @@
                 return val;
             };
             var set_num = function (index, val) {
-               if (utils.isArray(self.num)) {
-                   self.num[index] = val;
-                   return;
-               }
+                if (utils.isArray(self.num)) {
+                    self.num[index] = val;
+                    return;
+                }
 
                 self.num = val;
             }
+            var set_default_index = function (arr) {
+                var _arr = [];
+                for (var i = 0; i < arr.length; i++) {
+                    _arr.push(i);
+                }
 
+                return _arr;
+            }
+
+            if (!arguments.length) return;
+            if (arguments.length === 1) {
+                percent = index;
+                if (is_arr) {
+                    index = set_default_index(percent);
+                }
+            }
+            
             if (is_arr) {
                 for (var i = 0; i < index.length; i++) {
                     var _p = utils.getText(percent, i);
                     var _i = is_num(index[i]);
                     set_num(_i, _p);
-                    // this.num[_i] = _p;
                     this.init();
                 }
             } else {
                 index = is_num(index);
-                set_num(index, percent);
+                set_num(index, utils.getText(percent, 0));
                 this.init();
             }
         }
     }
 
     window.ProcessCircle = ProcessCircle;
-	
-}) (window, document);
+
+})(window, document);
